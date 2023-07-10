eb08a95b23c58d4246d4850f678b4b029a4641ca
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-07-10 12:54:53 -0600

    Validar solicitudes

--- hermes/mensajes/app.py
+++ hermes/mensajes/app.py
@@ -33,19 +33,20 @@ def consultar():
 
     # Mostrar los datos del mensaje
     mensaje = json.loads(mensaje_str)
-    rich.print(f"Archivo: {mensaje['archivo']}")
     rich.print(f"Correo electrónico: {mensaje['correo_electronico']}")
     rich.print(f"Depósito: {mensaje['deposito']}")
+    rich.print(f"Estado: {mensaje['estado']}")
     rich.print(f"Token: {mensaje['token']}")
+    rich.print(f"URL: {mensaje['url']}")
     raise typer.Exit(code=0)
 
 
 @app.command()
 def crear(
-    archivo: str,
     correo_electronico: str,
     deposito: str,
     token: str,
+    url: str,
 ):
     """
     Crear un mensaje
@@ -54,10 +55,11 @@ def crear(
 
     # Crear un diccionario con los datos del mensaje
     mensaje = {
-        "archivo": archivo,
         "correo_electronico": correo_electronico,
         "deposito": deposito,
+        "estado": "PENDIENTE",
         "token": token,
+        "url": url,
     }
 
     # Guardar el mensaje en la cola
@@ -65,3 +67,27 @@ def crear(
 
     # Mostrar mensaje de éxito
     rich.print("[green]Mensaje creado[/green]")
+
+
+@app.command()
+def enviar():
+    """
+    Enviar los mensajes pendientes
+    """
+    rich.print("Enviar los mensajes pendientes")
+
+    # Obtener todos los mensajes pendientes
+    mensajes = redis_client.lrange(QUEUE, 0, -1)
+
+    # Si no hay mensajes
+    if not mensajes:
+        rich.print("[red]No hay mensajes[/red]")
+        raise typer.Exit(code=0)
+
+    # Bucle con los mensajes
+    for mensaje_str in mensajes:
+        mensaje = json.loads(mensaje_str)
+        rich.print(f"Para {mensaje['correo_electronico']}")
+        rich.print(f"Asunto: Archivo que solicitaste")
+        rich.print(f"Contenido: Buen día, aquí está el archivo que solicitaste: {mensaje['url']}")
+        rich.print("")
--- hermes/solicitudes/app.py
+++ hermes/solicitudes/app.py
@@ -9,11 +9,14 @@ Se validan los campos y si cumple con los requisitos se converten en Mensajes.
 
 """
 import json
+import re
 
 import rich
 import typer
 
+from config.settings import GCS_BUCKETS
 from lib.redis import redis_client
+from lib.safe_string import EMAIL_REGEXP, URL_PDF_FILE_REGEXP
 
 QUEUE = "hermes_solicitudes"
 
@@ -21,11 +24,54 @@ app = typer.Typer()
 
 
 @app.command()
-def consultar():
+def crear(
+    correo_electronico: str,
+    deposito: str,
+    token: str,
+    url: str,
+):
     """
-    Consultar solicitudes
+    Crear una solicitud
     """
-    rich.print("Consultar solicitudes")
+    rich.print("Crear una solicitud")
+
+    # Validar el correo electrónico
+    if not re.match(EMAIL_REGEXP, correo_electronico):
+        rich.print("[red]El correo electrónico no es válido[/red]")
+        raise typer.Exit(code=1)
+
+    # Validar el URL al archivo PDF
+    if not re.match(URL_PDF_FILE_REGEXP, url):
+        rich.print("[red]El URL no es válido[/red]")
+        raise typer.Exit(code=1)
+
+    # Validar el depósito
+    if deposito not in GCS_BUCKETS:
+        rich.print("[red]El depósito no existe[/red]")
+        raise typer.Exit(code=1)
+
+    # Crear un diccionario con los datos de la solicitud
+    solicitud = {
+        "correo_electronico": correo_electronico,
+        "deposito": deposito,
+        "token": token,
+        "url": url,
+    }
+
+    # Guardar la solicitud en la cola
+    redis_client.lpush(QUEUE, json.dumps(solicitud))
+
+    # Mensaje de éxito
+    rich.print("[green]Solicitud creada[/green]")
+    raise typer.Exit(code=0)
+
+
+@app.command()
+def validar():
+    """
+    Validar solicitudes para crear mensajes
+    """
+    rich.print("Validar solicitudes para crear mensajes")
 
     # Obtener y eliminar el primer elemento de la cola
     solicitud_str = redis_client.rpop(QUEUE)
@@ -35,38 +81,44 @@ def consultar():
         rich.print("[red]No hay solicitudes[/red]")
         raise typer.Exit(code=0)
 
-    # Mostrar los datos de la solicitud
-    solicitud = json.loads(solicitud_str)
-    rich.print(f"Archivo: {solicitud['archivo']}")
-    rich.print(f"Correo electrónico: {solicitud['correo_electronico']}")
-    rich.print(f"Depósito: {solicitud['deposito']}")
-    rich.print(f"Token: {solicitud['token']}")
+    # Bucle por las solicitudes
+    while solicitud_str:
+        # Cargar datos de la solicitud
+        solicitud = json.loads(solicitud_str)
+
+        # Mostrar sus datos
+        rich.print(f"Correo electrónico: {solicitud['correo_electronico']}")
+        rich.print(f"Depósito: {solicitud['deposito']}")
+        rich.print(f"Token: {solicitud['token']}")
+        rich.print(f"URL: {solicitud['url']}")
+        rich.print("")
+
+        # Obtener y eliminar el primer elemento de la cola
+        solicitud_str = redis_client.rpop(QUEUE)
+
+    # Mensaje de éxito
+    rich.print("[green]Solicitudes validadas a mensajes[/green]")
     raise typer.Exit(code=0)
 
 
 @app.command()
-def crear(
-    archivo: str,
-    correo_electronico: str,
-    deposito: str,
-    token: str,
-):
+def limpiar():
     """
-    Crear una solicitud
+    Limpiar todas las solicitudes
     """
-    rich.print("Crear una solicitud")
+    rich.print("Limpiar todas las solicitudes")
 
-    # Crear un diccionario con los datos de la solicitud
-    solicitud = {
-        "archivo": archivo,
-        "correo_electronico": correo_electronico,
-        "deposito": deposito,
-        "token": token,
-    }
+    # Obtener la cantidad de solicitudes
+    count = redis_client.llen(QUEUE)
 
-    # Guardar la solicitud en la cola
-    redis_client.lpush(QUEUE, json.dumps(solicitud))
+    # Si no hay solicitudes
+    if not count:
+        rich.print("[red]No hay solicitudes[/red]")
+        raise typer.Exit(code=0)
 
-    # Mensaje de éxito
-    rich.print("[green]Solicitud creada[/green]")
+    # Eliminar todas las solicitudes
+    redis_client.delete(QUEUE)
+
+    # Mostrar mensaje de éxito
+    rich.print(f"[green]Se eliminaron {count} solicitudes[/green]")
     raise typer.Exit(code=0)
